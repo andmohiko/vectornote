91ab54cfd8c2c7ea647376bfc55df8cdaa0dfe9d
andmohiko <andmohiko@gmail.com>
2026-03-23 13:09:42 +0900

    fix: pkg name and delete unused apps

--- apps/pageRouter/src/components/Base/BorderLine/index.tsx
+++
@@ -1,5 +0,0 @@
-import styles from './style.module.css'
-
-export const BorderLine = (): React.ReactElement => {
-  return <hr className={styles.borderLine} />
-}
--- apps/pageRouter/src/components/Base/PageHead.tsx
+++
@@ -1,9 +0,0 @@
-import Head from 'next/head'
-
-export const PageHead = (): React.ReactElement => (
-  <Head>
-    <title>Firebase Monorepo</title>
-    <meta name="description" content="Firebase Monorepo" />
-    <link rel="icon" href="/favicon.ico" />
-  </Head>
-)
